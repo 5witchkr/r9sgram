7479fc6dc435ce25cc8de8cc9304e9f4d5ba3f81
5witchkr <hob4419@gmail.com>
2022-04-16 21:59:10 +0900

    use widget-materialapp,scafflod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    //materialapp위젯 사용함 (option : title, theme, home)
+    MaterialApp(
+      //option
+      home: MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //scafflod 위젯 리턴
+    return Scaffold(
+      //appbar (네비바)
+      appBar: AppBar(),
+      //본문에 text
+      body: Text('text'),
+    );
   }
 }
