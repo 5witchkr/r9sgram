d5cbb18d106093f41e41ba64c78be5f497355b4c
5witchkr <hob4419@gmail.com>
2022-04-16 22:30:28 +0900

    add style.dart, make navBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import './style.dart' as style;
+
 
 void main() {
   runApp(
     //materialapp위젯 사용함 (option : title, theme, home)
     MaterialApp(
       //option
+      //theme? css같은 역할
+      theme: style.themeAppBar,
       home: MyApp()
     )
   );
@@ -18,9 +22,17 @@ class MyApp extends StatelessWidget {
     //scafflod 위젯 리턴
     return Scaffold(
       //appbar (네비바)
-      appBar: AppBar(),
+      appBar: AppBar(
+          title: Text('R9Sgram'),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.add_box_outlined),
+              iconSize: 30,
+            ),
+          ]),
       //본문에 text
-      body: Text('text'),
+      body: Text("hello"),
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+var themeAppBar = ThemeData(
+  appBarTheme: AppBarTheme(
+    color: Colors.blueGrey,
+    elevation: 2,
+    titleTextStyle: TextStyle(color: Colors.white, fontSize: 25),
+  ),
+);
